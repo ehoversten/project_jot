8a295a32b7ed6af6de6a4b77a0d79d8aadc5bf8d
ehoversten <ehoversten@gmail.com>
2019-03-08 23:35:14 -0500

    added express handlebars middlewear and bootstrap4 dependentcies

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
+// --- LOAD MODULES --- //
+
 // Load the express module
 const express = require('express');
+// Load Express Handlebars Module
+const exphbs = require('express-handlebars');
 
 // Invoke Express
 const app = express();
+
+// --- MIDDLEWEAR --- //
+
+// Express-Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+// --- ROUTING --- //
+
+// INDEX ROUTE
+app.get('/', (req, res) => {
+    const title = 'INDEX'
+    res.render('index', {
+        title: title
+    });
+});
+
+// ABOUT ROUTE
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// --- SERVER --- //
 
 // Declare a port 
 const PORT = 5000;
