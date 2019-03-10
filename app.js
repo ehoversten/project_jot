3974f3dd657ef6c605c49480da7ae3c75a9bd7bf
ehoversten <ehoversten@gmail.com>
2019-03-09 19:22:25 -0500

    added delete idea from database functionality

--- app.js
+++ app.js
@@ -148,6 +148,14 @@ app.put('/ideas/:id', (req, res) => {
     });
 });
 
+// DELETE IDEA
+app.delete('/ideas/:id', (req, res) => {
+    Idea.remove({_id: req.params.id})
+        .then(() => {
+            res.redirect('/ideas')
+        });
+});
+
 // --- SERVER --- //
 
 // Declare a port
