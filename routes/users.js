53d501167be77ba986b25d9c2ee73f9e3589d4fe
ehoversten <ehoversten@gmail.com>
2019-03-10 18:05:56 -0400

    added process register (post route) functionality and basic validation

--- routes/users.js
+++ routes/users.js
@@ -19,8 +19,38 @@ router.get('/login', (req, res) => {
   res.render('users/login');
 });
 
+// User Registration Route
 router.get('/register', (req, res) => {
   res.render('users/register');
+});
+
+// Register form PROCESS
+router.post('/register', (req, res) => {
+    // TESTING
+    // console.log(req.body);
+    // res.send("Registered");
+
+    let errors = [];
+
+    if(req.body.password != req.body.password2) {
+        errors.push({text:"Passwords do not match"});
+    }
+
+    if(req.body.password.length < 4) {
+        errors.push({text: "Password must be greater than 4 characters"});
+    }
+
+    if(errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        });
+    } else {
+        res.send("User Registered");
+    }
 });
 
 
